0b4ad69c0285d44fd777b6ecf90cfb65a7ffcd97
Александр <a40546b@gmail.com>
2024-03-15 12:27:36 +0300

    first commit

---
+++ HW-7.1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные 
+// числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+
+Console.WriteLine("Задайте натуральные M и N, и мы выведем все натуральные числа в промежутке от M до N:");
+int numM = int.Parse(Console.ReadLine()!);
+int numN = int.Parse(Console.ReadLine()!);
+
+void PrintRangeNumbers(int numM, int numN)
+{
+    if (numM < 1 | numN < 1)
+    {
+        Console.WriteLine("| натуральные числа - это положительные целые числа от 1 до бесконечности");
+        return;  // не комментить! Сигнал для прерывания
+    }
+    if (numM <= numN) 
+    {
+        Console.WriteLine(numM);
+        PrintRangeNumbers(numM + 1, numN);  
+    }
+}
+
+PrintRangeNumbers(numM, numN);
---
+++ HW-7.2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+
+int numA = EnterInt("Введите неотрицательное целое число A: ");
+int numB = EnterInt("Введите неотрицательное целое число B: ");
+
+int EnterInt(string prompt)
+{
+    Console.Write(prompt);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int AckermanFunction(int numA, int numB)
+{
+    if (numA == 0)
+        return numB + 1;
+    if (numA > 0 && numB == 0)
+        return AckermanFunction(numA - 1, 1);
+    return AckermanFunction(numA - 1, AckermanFunction(numA, numB - 1));
+}
+
+Console.WriteLine($"Функция Аккермана для {numA} и {numB} составляет {AckermanFunction(numA, numB)}");
+Console.ReadLine();
